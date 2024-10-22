8bcb4641a1bce99689a727960629bf34de502c5e
AYAZ MAHMOOD <hmayaz528@gmail.com>
2024-10-23 01:33:28 +0500

    fet: data polling

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -6,15 +6,33 @@ const fetchSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const onSuccess = (data) => {
+    console.log("Perform side effects after data fetching", data);
+  };
+
+  const onError = (error) => {
+    console.log("Perform data fetching after encountering error", error);
+  };
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heros",
     fetchSuperHeroes,
-    { cacheTime: 5000 }
+    {
+      onSuccess: onSuccess,
+      onError: onError,
+    }
+    // {
+    //   enabled: false, // data will not be fetched on mount
+    // }
+    // { cacheTime: 5000 }
+    // { staleTime: 30000 }
+    // { refetchOnMount: true },
+    // { refetchOnWindowFocus: true }, // query will be in sync with the remote data(no need to refresh the page)
+    // { refetchInterval: 2000, refetchIntervalInBackground: true } // polling: data will be fetched every 2 sec
   );
 
   console.log("isLoading - isFetching", isLoading, isFetching);
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -25,6 +43,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
       })}
